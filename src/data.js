a3802ce48ae5325d78b9017250765f8afd22eebc
Gergely Bence Nagy <gergely_bence_nagy@epam.com>
2021-05-13 09:49:30 +0200

    Add subType and Level to data

--- src/data.js
+++ src/data.js
@@ -2,59 +2,83 @@
 export default [
     {
         type: "Soft Skill",
+        subType: "Subtype Name",
+        level: "L3",
         question: "How would you explain a complex topic to a new customer or a colleague?",
-        answer: "The purpose of this question is to test the candidate’s communication skills. Communication is one of the crucial soft skills that a candidate should have. The person might be good when it comes to the job but it won’t help the organization when they can’t communicate effectively.",
+        answer:
+            "The purpose of this question is to test the candidate’s communication skills. Communication is one of the crucial soft skills that a candidate should have. The person might be good when it comes to the job but it won’t help the organization when they can’t communicate effectively.",
     },
     {
         type: "Soft Skill",
+        subType: "Subtype Name",
+        level: "L3",
         question: "How did you handle an urgent problem when your supervisor or manager was away?",
-        answer: "This question is all about measuring the growth potential of the candidate. It is asked in order to gauge if the candidate is able to step up when called upon to make some important decisions. A good employee will know what to do when faced with such a challenge. You also want to get their perspective when it comes to being proactive in an organization setting.",
+        answer:
+            "This question is all about measuring the growth potential of the candidate. It is asked in order to gauge if the candidate is able to step up when called upon to make some important decisions. A good employee will know what to do when faced with such a challenge. You also want to get their perspective when it comes to being proactive in an organization setting.",
     },
     {
         type: "Soft Skill",
+        subType: "Subtype Name",
+        level: "L3",
         question: "What if you fail this asmt session?",
         answer: "I hope I will receive some information about what to improve and I will do my very best to do so and will try again later.",
     },
     {
         type: "Soft Skill",
+        subType: "Subtype Name",
+        level: "L3",
         question: "What kind of senior do you want to be? What skills do you need?",
         answer: "Proper technical knowledge. Proper attitude. Knows how to talk and what he or she wants to talk about",
     },
     {
         type: "Soft Skill",
+        subType: "Subtype Name",
+        level: "L3",
         question: "What motivates you?",
         answer:
             "I believe it is mostly challenging tasks motivate me. I like to learn new things, dig into a problem, because if I manage to resolve a challenging problem, the feel of success is really good after.",
     },
     {
         type: "Soft Skill",
+        subType: "Subtype Name",
+        level: "L3",
         question: "What do you do if there's a disagreement within your team?",
-        answer:
-            "Separate the two debating person, let them cool down, have a talk and a coffee with both of them to know the circumstances.",
+        answer: "Separate the two debating person, let them cool down, have a talk and a coffee with both of them to know the circumstances.",
     },
     {
         type: "Soft Skill",
+        subType: "Subtype Name",
+        level: "L3",
         question: "Your trainee asked you a question and you don't know the answer, what will you do in this case?",
         answer: "Tell him that I don’t know it and will look into it and provide an answer as soon as possible.",
     },
     {
         type: "Soft Skill",
+        subType: "Subtype Name",
+        level: "L3",
         question: "What kind of estimation techniques are you aware of?",
-        answer: "Time-based: manhour or manday. Good for estimation when the result depends on how many people working on it - tasks which we can work on in parallel (regression). Complexity-based: has nothing to do with time, although they tend to try to act like it has as the sprint also has an ending. How complex a task is. Story point: usually we estimate in story points, Fibonacci-numbers (good for time-based too). Can be hard, because it can lead to misunderstandings. (Is it a 3 or an 8?) - reference stories are good samples. Works good if it is specified what we mean by what story point. Planning poker: key is not to influence others. Minimum and maximum values are discussed, everybody should agree on at the end. t-shirt sizing: good when the project starts and we create a roadmap",
+        answer:
+            "Time-based: manhour or manday. Good for estimation when the result depends on how many people working on it - tasks which we can work on in parallel (regression). Complexity-based: has nothing to do with time, although they tend to try to act like it has as the sprint also has an ending. How complex a task is. Story point: usually we estimate in story points, Fibonacci-numbers (good for time-based too). Can be hard, because it can lead to misunderstandings. (Is it a 3 or an 8?) - reference stories are good samples. Works good if it is specified what we mean by what story point. Planning poker: key is not to influence others. Minimum and maximum values are discussed, everybody should agree on at the end. t-shirt sizing: good when the project starts and we create a roadmap",
     },
     {
         type: "Soft Skill",
+        subType: "Subtype Name",
+        level: "L3",
         question: "How would you estimate?",
         answer: "In a perfect world, have someone from all sides (dev, QA, business), so that everybody understands everything, business can ask. Estimate together, maybe with planning poker.",
     },
     {
         type: "Soft Skill",
+        subType: "Subtype Name",
+        level: "L3",
         question: "Can you tell me about agile methodologies?",
         answer:
             "Agile methodologies are based on an incremental iterative approach. Self-organized teams do the work. Agile methodologies are open to changing requirements over time and encourages constant feedback from the end users.",
     },
     {
         type: "Soft Skill",
+        subType: "Subtype Name",
+        level: "L3",
         question: "How do you keep your knowledge up to date? What have you read recently?",
         answer: `I took part in a self-organized so-called mentoring program to support me with my preparation for ASMT. I do online trainings. I read blogs and articles.`,
     },
