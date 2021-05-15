4579b99cfa94e5f4b87199fab1ad7b89487bf55f
Gergely Bence Nagy <gergely_bence_nagy@epam.com>
2021-05-15 18:50:53 +0200

    Rethink Card UI style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
-const colors = require('tailwindcss/colors')
+// const colors = require('tailwindcss/colors')
 module.exports = {
     purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
           colors: {
-            cyan: colors.cyan
+            'answerix-light-blue-50': 'rgb(196	215	232)',
+            'answerix-light-blue-100': 'rgb(169	208	235)',
+            'answerix-white': 'rgb(241	244	251)',
           }
         },
     },
