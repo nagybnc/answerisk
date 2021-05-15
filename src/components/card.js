3af39fb145b2c9f0ed1368a22bb30d2143d206a2
Gergely Bence Nagy <gergely_bence_nagy@epam.com>
2021-05-15 18:56:59 +0200

    Add Card Main content

--- src/components/card.js
+++ src/components/card.js
@@ -2,8 +2,10 @@ import React from "react";
 import { animated, to } from "react-spring";
 
 const Card = ({ i, x, y, rot, scale, trans, bind, data }) => {
-    // const { subType, question, answer } = data[i];
+    const { subType, question, answer } = data[i];
     // const [showAnswer, setShowAnswer] = React.useState(false);
+    const calculatedProgressLength = ((i + 1) / data.length) * 100;
+    
     return (
         <animated.div
             key={i}
@@ -19,6 +21,24 @@ const Card = ({ i, x, y, rot, scale, trans, bind, data }) => {
                 }}
                 className="deck-body font-mono rounded-lg bg-answerix-white"
             >
+                <header className="h-8 w-full mb-8 rounded-t-lg bg-answerix-dark-blue-800"></header>
+
+                <main className="h-card mx-6 pt-6 px-6 bg-white shadow-lg">
+                    <p className="text-5xl text-gray-700">
+                        {i + 1}
+                        <span className="text-lg text-gray-400"> OF {data.length}</span>
+                    </p>
+                    <div className="relative pt-1">
+                        <div className="text-xs h-2 mb-4 overflow-hidden flex rounded bg-teal-100">
+                            <div style={{ width: `${calculatedProgressLength}%` }} className="bg-answerix-emerald-500"></div>
+                        </div>
+                    </div>
+                    <article className="h-80 py-2">
+                        <p className="text-xl font-bold mb-2 text-answerix-emerald-500">{question}</p>
+                        {answer}
+                    </article>
+                </main>
+
             </animated.div>
         </animated.div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
-// const colors = require('tailwindcss/colors')
+const colors = require('tailwindcss/colors')
 module.exports = {
     purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
           colors: {
+            teal: colors.teal,
             'answerix-light-blue-50': 'rgb(196	215	232)',
             'answerix-light-blue-100': 'rgb(169	208	235)',
             'answerix-white': 'rgb(241	244	251)',
+            'answerix-dark-blue-800': 'rgb(69	107	190)',
+            'answerix-emerald-500': 'rgb(105	202	181)',
           }
         },
     },
